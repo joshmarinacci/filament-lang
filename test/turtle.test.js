ddfc721a3e7486161fd0d7c8150840dc19497a57
Josh Marinacci <jmarinacci@lyft.com>
2021-02-17 16:00:45 -0800

    add some turtle examples

---
+++ test/turtle.test.js
@@ -0,0 +1,64 @@
+import {promises as fs, createWriteStream, mkdir as real_mkdir} from 'fs'
+import {default as path} from 'path'
+import {setup} from './common.js'
+import {eval_code} from '../src/index.js'
+import {default as PImage} from 'pureimage'
+
+await setup()
+
+async function mkdir(dir) {
+    return new Promise((res,rej)=>{
+        real_mkdir(dir,(err)=>{
+            if(err) {
+                // console.log(err)//return rej(err)
+            }
+            res()
+        })
+    })
+}
+
+async function code_to_png(code, fname) {
+    // console.log("parsing",code)
+    await mkdir('output_turtle')
+    // console.log("rendering to ",fname)
+    let ret = await eval_code(code)
+    const img = PImage.make(500,500);
+    await ret.cb(img)
+    await PImage.encodePNGToStream(img,createWriteStream(path.join('output_turtle',fname)))
+    // console.log("done rendering")
+
+}
+
+describe('turtle basics',() => {
+    it('make square',async ()=> {
+        await code_to_png(`{
+        turtle_start(0,0,0)
+        turtle_pendown()
+        turtle_forward(10)
+        turtle_right(90)
+        turtle_forward(10)
+        turtle_right(90)
+        turtle_forward(10)
+        turtle_right(90)
+        turtle_forward(10)
+        turtle_right(90)
+        turtle_penup()
+        turtle_done()
+        }`,"turtle_square.png")
+    })
+
+    it('makes spiral', async() => {
+        await code_to_png(`{
+        turtle_start(0,0,0)
+        turtle_pendown()
+        def part(i:?) {
+            turtle_forward(i * 10)
+            turtle_right(144)
+        }
+        map(range(20),with:part)
+        turtle_penup()
+        turtle_done()
+        }`,'turtle_spiral_star.png')
+    })
+
+})
