5ff2e3ee382894c8e44c79847c9409dfdeeae3d6
Josh Marinacci <jmarinacci@lyft.com>
2021-02-17 18:48:46 -0800

    more examples

--- test/turtle.test.js
+++ test/turtle.test.js
@@ -80,4 +80,55 @@ describe('turtle basics',() => {
         turtle_done()
         }`,'turtle_polygon.png')
     })
+
+    /*
+
+    https://www.calormen.com/jslogo/#
+
+https://personal.utdallas.edu/~veerasam/logo/
+
+http://logo.twentygototen.org/dZ1f62XY
+
+
+    tree
+    to tree :size
+   if :size < 5 [forward :size back :size stop]
+   forward :size/3
+   left 30 tree :size*2/3 right 30
+   forward :size/6
+   right 25 tree :size/2 left 25
+   forward :size/3
+   right 25 tree :size/2 left 25
+   forward :size/6
+   back :size
+end
+clearscreen
+tree 150
+
+
+
+
+to fern :size :sign
+  if :size < 1 [ stop ]
+  fd :size
+  rt 70 * :sign fern :size * 0.5 :sign * -1 lt 70 * :sign
+  fd :size
+  lt 70 * :sign fern :size * 0.5 :sign rt 70 * :sign
+  rt 7 * :sign fern :size - 1 :sign lt 7 * :sign
+  bk :size * 2
+end
+window clearscreen pu bk 150 pd
+fern 25 1
+
+
+random rects
+to square :length
+  repeat 4 [ fd :length rt 90 ]
+end
+to randomcolor
+  setcolor pick [ red orange yellow green blue violet ]
+end
+clearscreen
+repeat 36 [ randomcolor square random 200 rt 10 ]
+     */
 })
