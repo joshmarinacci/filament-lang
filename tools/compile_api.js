dda60fa967ec334f74cc65b304a263105fdaa1ab
Josh Marinacci <jmarinacci@lyft.com>
2021-03-12 13:49:00 -0800

    add tool to compile the api into json and js file

---
+++ tools/compile_api.js
@@ -0,0 +1,37 @@
+#!/usr/bin/env node
+import {promises as fs} from 'fs'
+import {parse_api_docs} from './api_parser.js'
+import {group_modules} from './api_generator.js'
+
+const SRC = [
+    'src/chart.js',
+    'src/math.js',
+    'src/lists.js',
+]
+
+
+export async function for_each(files, process_file) {
+    for (const file of files) {
+        await process_file(file)
+    }
+}
+
+async function make_api_docs(files) {
+    let apis = []
+    await for_each(files, async (file) => {
+        // l("parsing api from", file)
+        let raw = (await fs.readFile(file)).toString()
+        let defs = await parse_api_docs(raw)
+        apis = apis.concat(defs)
+    })
+
+    let modules = group_modules(apis)
+    let json = JSON.stringify(modules, null, "    ")
+    // await fs.writeFile('output/api.json', json)
+    // await generate_api_html('output/api.generated.html',modules)
+    const js = `export default String.raw\`${json}\``;
+    await fs.writeFile('src/api.json.js',js)
+}
+
+
+await make_api_docs(SRC)
